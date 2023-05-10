f50b21159641ad770806d1c13a7ed1c8569211af
CanftIn <wwc7033@gmail.com>
2023-05-10 04:45:33 +0000

    update TokenizedBuffer test

--- unittests/TokenizedBuffer.t.cc
+++ unittests/TokenizedBuffer.t.cc
@@ -18,7 +18,6 @@ namespace {
 using namespace Cocktail;
 
 struct LexerTest : ::testing::Test {
-
   auto GetSourceBuffer(llvm::Twine text) -> SourceBuffer& {
     auto buffer_expected = SourceBuffer::CreateFromText(text.str());
     if (auto err = buffer_expected.takeError()) {
@@ -36,6 +35,11 @@ TEST_F(LexerTest, HandlesEmptyBuffer) {
   auto buffer = Lex("");
   EXPECT_FALSE(buffer.HasErrors());
   EXPECT_EQ(buffer.Tokens().begin(), buffer.Tokens().end());
+}
+
+TEST_F(LexerTest, TracksLinesAndColumns) {
+  auto buffer = Lex("\n  ;;\n   ;;;\n");
+  EXPECT_FALSE(buffer.HasErrors());
 }
 
 }  // namespace
