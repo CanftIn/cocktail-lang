83d736a79c85ffa483ec959811ff2d06dd596916
CanftIn <wwc7033@gmail.com>
2023-05-23 14:40:03 +0800

    update diagnostic

--- include/Cocktail/Diagnostics/DiagnosticEmitter.h
+++ include/Cocktail/Diagnostics/DiagnosticEmitter.h
@@ -22,16 +22,19 @@ class DiagnosticEmitter {
   using Callback = std::function<void(const Diagnostic&)>;
 
   explicit DiagnosticEmitter(Callback callback)
-      : callback_(std::move(callback)) {}
+      : callback(std::move(callback)) {}
   ~DiagnosticEmitter() = default;
 
   template <typename DiagnosticT>
-  void EmitError(
-      llvm::function_ref<void(typename DiagnosticT::Substitutions&)> f) {
-    typename DiagnosticT::Substitutions substitutions;
-    f(substitutions);
-    callback_({.short_name = DiagnosticT::ShortName,
-               .message = DiagnosticT::Format(substitutions)});
+  void EmitError(typename DiagnosticT::Substitutions substitutions) {
+    callback({.short_name = DiagnosticT::ShortName,
+              .message = DiagnosticT::Format(substitutions)});
+  }
+
+  template <typename DiagnosticT>
+  std::enable_if_t<std::is_empty_v<typename DiagnosticT::Substitutions>>
+  EmitError() {
+    EmitError<DiagnosticT>({});
   }
 
   template <typename DiagnosticT>
@@ -39,31 +42,34 @@ class DiagnosticEmitter {
       llvm::function_ref<bool(typename DiagnosticT::Substitutions&)> f) {
     typename DiagnosticT::Substitutions substitutions;
     if (f(substitutions)) {
-      callback_({.short_name = DiagnosticT::ShortName,
-                 .message = DiagnosticT::Format(substitutions)});
+      callback({.short_name = DiagnosticT::ShortName,
+                .message = DiagnosticT::Format(substitutions)});
     }
   }
 
  private:
-  Callback callback_;
+  Callback callback;
 };
 
 inline auto ConsoleDiagnosticEmitter() -> DiagnosticEmitter& {
-  static auto* emitter =
-      new DiagnosticEmitter([](const Diagnostic& diagnostic) {
-        llvm::errs() << diagnostic.short_name << ": " << diagnostic.message
-                     << "\n";
-      });
+  static auto* emitter = new DiagnosticEmitter(
+      [](const Diagnostic& d) { llvm::errs() << d.message << "\n"; });
   return *emitter;
 }
 
 inline auto NullDiagnosticEmitter() -> DiagnosticEmitter& {
-  static auto* emitter =
-      new DiagnosticEmitter([](const Diagnostic& diagnostic) {
-        // Do nothing.
-      });
+  static auto* emitter = new DiagnosticEmitter([](const Diagnostic&) {});
   return *emitter;
 }
+
+// CRTP base class for diagnostics with no substitutions.
+template <typename Derived>
+struct SimpleDiagnostic {
+  struct Substitutions {};
+  static auto Format(const Substitutions&) -> std::string {
+    return Derived::Message.str();
+  }
+};
 
 }  // namespace Cocktail
 
