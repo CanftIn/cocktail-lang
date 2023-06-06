0a4e721f70bf1fce1f842a7270b90867bddb4047
CanftIn <wwc7033@gmail.com>
2023-06-06 16:44:36 +0800

    update common check

--- include/Cocktail/Common/Check.h
+++ include/Cocktail/Common/Check.h
@@ -7,11 +7,66 @@
 
 namespace Cocktail {
 
-#define CHECK(CONDITION)                                    \
-  if (!(CONDITION)) {                                       \
-    llvm::sys::PrintStackTrace(llvm::errs());               \
-    llvm::report_fatal_error("CHECK failure: " #CONDITION); \
+// Wraps a stream and exiting for fatal errors.
+class ExitingStream {
+ public:
+  ~ExitingStream() {
+    // Finish with a newline.
+    llvm::errs() << "\n";
+    if (treat_as_bug) {
+      std::abort();
+    } else {
+      std::exit(-1);
+    }
   }
+
+  // Indicates that initial input is in, so this is where a ": " should be added
+  // before user input.
+  auto AddSeparator() -> ExitingStream& {
+    separator = true;
+    return *this;
+  }
+
+  // Indicates that the program is exiting due to a bug in the program, rather
+  // than, e.g., invalid input.
+  auto TreatAsBug() -> ExitingStream& {
+    treat_as_bug = true;
+    return *this;
+  }
+
+  // If the bool cast occurs, it's because the condition is false. This supports
+  // && short-circuiting the creation of ExitingStream.
+  explicit operator bool() const { return true; }
+
+  // Forward output to llvm::errs.
+  template <typename T>
+  auto operator<<(const T& message) -> ExitingStream& {
+    if (separator) {
+      llvm::errs() << ": ";
+      separator = false;
+    }
+    llvm::errs() << message;
+    return *this;
+  }
+
+ private:
+  // Whether a separator should be printed if << is used again.
+  bool separator = false;
+
+  // Whether the program is exiting due to a bug.
+  bool treat_as_bug = false;
+};
+
+// For example:
+//   CHECK(is_valid) << "Data is not valid!";
+#define CHECK(condition)                                                      \
+  (!(condition)) && (Cocktail::ExitingStream() << "CHECK failure: " #condition) \
+                        .AddSeparator()                                       \
+                        .TreatAsBug()
+
+// For example:
+//   FATAL() << "Unreachable!";
+#define FATAL() Cocktail::ExitingStream().TreatAsBug() << "FATAL: "
 
 }  // namespace Cocktail
 
--- unittests/Common/Check.t.cc
+++ unittests/Common/Check.t.cc
@@ -9,7 +9,38 @@ using namespace Cocktail;
 TEST(CheckTest, CheckTrue) { CHECK(true); }
 
 TEST(CheckTest, CheckFalse) {
-  ASSERT_DEATH({ CHECK(false); }, "LLVM ERROR: CHECK failure: false");
+  ASSERT_DEATH({ CHECK(false); }, "CHECK failure: false\n");
+}
+
+TEST(CheckTest, CheckTrueCallbackNotUsed) {
+  bool called = false;
+  auto callback = [&]() {
+    called = true;
+    return "called";
+  };
+  CHECK(true) << callback();
+  EXPECT_FALSE(called);
+}
+
+TEST(CheckTest, CheckFalseMessage) {
+  ASSERT_DEATH({ CHECK(false) << "msg"; }, "CHECK failure: false: msg\n");
+}
+
+TEST(CheckTest, CheckOutputForms) {
+  const char msg[] = "msg";
+  std::string str = "str";
+  int i = 1;
+  CHECK(true) << msg << str << i << 0;
+}
+
+TEST(CheckTest, Fatal) {
+  ASSERT_DEATH({ FATAL() << "msg"; }, "FATAL: msg\n");
+}
+
+auto FatalNoReturnRequired() -> int { FATAL() << "msg"; }
+
+TEST(ErrorTest, FatalNoReturnRequired) {
+  ASSERT_DEATH({ FatalNoReturnRequired(); }, "FATAL: msg\n");
 }
 
 }  // namespace
