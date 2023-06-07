96bd880cace37cc616b22c91cdd2a21da150b0c0
CanftIn <wwc7033@gmail.com>
2023-06-07 11:29:16 +0800

    update fn inserter test

--- unittests/CppRefactor/FnInserter.t.cc
+++ unittests/CppRefactor/FnInserter.t.cc
@@ -19,4 +19,49 @@ TEST_F(FnInserterTest, TrailingReturn) {
   ExpectReplacement(Before, After);
 }
 
+TEST_F(FnInserterTest, Inline) {
+  constexpr char Before[] = "inline auto A() -> int;";
+  constexpr char After[] = "fn auto A() -> int;";
+  ExpectReplacement(Before, After);
+}
+
+TEST_F(FnInserterTest, Methods) {
+  // "virtual auto" should be "fn virtual"
+  constexpr char Before[] = R"cpp(
+    class Shape {
+     public:
+      virtual void Draw() = 0;
+      virtual auto NumSides() -> int = 0;
+    };
+
+    class Circle : public Shape {
+     public:
+      void Draw() override;
+      auto NumSides() -> int override;
+      auto Radius() -> double { return radius_; }
+
+     private:
+      double radius_;
+    };
+  )cpp";
+  constexpr char After[] = R"(
+    class Shape {
+     public:
+      virtual void Draw() = 0;
+      fn auto NumSides() -> int = 0;
+    };
+
+    class Circle : public Shape {
+     public:
+      void Draw() override;
+      fn NumSides() -> int override;
+      fn Radius() -> double { return radius_; }
+
+     private:
+      double radius_;
+    };
+  )";
+  ExpectReplacement(Before, After);
+}
+
 }  // namespace
