2fa8ca4e17af722382fbf858aedce67a80a1eff0
CanftIn <wwc7033@gmail.com>
2023-06-08 14:52:34 +0800

    update metaprogramming test

---
+++ unittests/Common/MetaProgramming.t.cc
@@ -0,0 +1,26 @@
+#include "Cocktail/Common/MetaProgramming.h"
+
+#include <gtest/gtest.h>
+
+#include "llvm/Support/raw_ostream.h"
+
+namespace {
+
+using namespace Cocktail;
+
+TEST(MetaProgrammingTest, RequiresBasic) {
+  bool result = Requires<int, int>([](int a, int b) { return a + b; });
+  EXPECT_TRUE(result);
+}
+
+struct TypeWithPrint {
+  void Print(llvm::raw_ostream& os) const { os << "Test"; }
+};
+
+TEST(MetaProgrammingTest, RequiresPrintMethod) {
+  bool result = Requires<const TypeWithPrint, llvm::raw_ostream>(
+      [](auto&& t, auto&& out) -> decltype(t.Print(out)) {});
+  EXPECT_TRUE(result);
+}
+
+}  // namespace
