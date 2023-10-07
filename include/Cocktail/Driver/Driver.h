d9942f29e2faa9c9b847a5800a397e4f87a328c4
CanftIn <wwc7033@gmail.com>
2023-10-07 18:00:09 +0800

    update Driver header

--- include/Cocktail/Driver/Driver.h
+++ include/Cocktail/Driver/Driver.h
@@ -3,38 +3,58 @@
 
 #include <cstdint>
 
-#include "Cocktail/Diagnostics/DiagnosticEmitter.h"
+#include "Cocktail/Common/CommandLine.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/Support/Debug.h"
+#include "llvm/Support/VirtualFileSystem.h"
 #include "llvm/Support/raw_ostream.h"
 
 namespace Cocktail {
 
+// Command line interface driver.
+//
+// Provides simple API to parse and run command lines for Carbon.  It is
+// generally expected to be used to implement command line tools for working
+// with the language.
 class Driver {
  public:
-  Driver() : output_stream_(llvm::outs()), error_stream_(llvm::errs()) {}
+  // Constructs a driver with any error or informational output directed to a
+  // specified stream.
+  Driver(llvm::vfs::FileSystem& fs, llvm::raw_pwrite_stream& output_stream,
+         llvm::raw_pwrite_stream& error_stream)
+      : fs_(fs), output_stream_(output_stream), error_stream_(error_stream) {
+    (void)fs_;
+  }
 
-  Driver(llvm::raw_ostream& output_stream, llvm::raw_ostream& error_stream)
-      : output_stream_(output_stream), error_stream_(error_stream) {}
-
-  auto RunFullCommand(llvm::ArrayRef<llvm::StringRef> args) -> bool;
-
-  auto RunHelpSubcommand(DiagnosticConsumer& consumer,
-                         llvm::ArrayRef<llvm::StringRef> args) -> bool;
-
-  auto RunDumpTokensSubcommand(DiagnosticConsumer& consumer,
-                               llvm::ArrayRef<llvm::StringRef> args) -> bool;
-
-  auto RunDumpParseTreeSubcommand(DiagnosticConsumer& consumer,
-                                  llvm::ArrayRef<llvm::StringRef> args) -> bool;
+  // Parses the given arguments into both a subcommand to select the operation
+  // to perform and any arguments to that subcommand.
+  //
+  // Returns true if the operation succeeds. If the operation fails, returns
+  // false and any information about the failure is printed to the registered
+  // error stream (stderr by default).
+  auto RunCommand(llvm::ArrayRef<llvm::StringRef> args) -> bool;
 
  private:
-  auto ReportExtraArgs(llvm::StringRef subcommand_text,
-                       llvm::ArrayRef<llvm::StringRef> args) -> void;
+  struct Options;
+  struct CompileOptions;
+  class CompilationUnit;
 
-  llvm::raw_ostream& output_stream_;
-  llvm::raw_ostream& error_stream_;
+  // Delegates to the command line library to parse the arguments and store the
+  // results in a custom `Options` structure that the rest of the driver uses.
+  auto ParseArgs(llvm::ArrayRef<llvm::StringRef> args, Options& options)
+      -> CommandLine::ParseResult;
+
+  // Does custom validation of the compile-subcommand options structure beyond
+  // what the command line parsing library supports.
+  auto ValidateCompileOptions(const CompileOptions& options) const -> bool;
+
+  // Implements the compile subcommand of the driver.
+  auto Compile(const CompileOptions& options) -> bool;
+
+  llvm::vfs::FileSystem& fs_;
+  llvm::raw_pwrite_stream& output_stream_;
+  llvm::raw_pwrite_stream& error_stream_;
+  llvm::raw_pwrite_stream* vlog_stream_ = nullptr;
 };
 
 }  // namespace Cocktail
